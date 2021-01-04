46e734103b1aec994631204823fcc879758820f3
1851951314 <1851951314@qq.com>
2021-01-04 14:41:29 +0800

    详情页评论区

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="detail">
+  <div class="detail" v-if="Object.keys(commentInfo).length !== 0">
     <detail-nav-bar class="detail-nav" />
     <scroll class="content" ref="scroll">
       <detail-swiper :top-images="topImages" />
@@ -10,6 +10,7 @@
         @loadImgEvent="loadImgEvent"
       />
       <detail-param-info :param-info="paramInfo" />
+      <detail-comment-info :comment-info="commentInfo" />
     </scroll>
   </div>
 </template>
@@ -21,6 +22,7 @@ import DetailBaseInfo from "./childComps/DetailBaseInfo.vue";
 import DetailShopInfo from "./childComps/DetailShopInfo.vue";
 import DetailGoodsInfo from "./childComps/DetailGoodsInfo.vue";
 import DetailParamInfo from "./childComps/DetailParamInfo.vue";
+import DetailCommentInfo from "./childComps/DetailCommentInfo.vue";
 
 import Scroll from "components/common/scroll/Scroll.vue";
 
@@ -36,6 +38,7 @@ export default {
       shop: {},
       detailInfo: {},
       paramInfo: {},
+      commentInfo: {},
     };
   },
   components: {
@@ -45,6 +48,7 @@ export default {
     DetailShopInfo,
     DetailGoodsInfo,
     DetailParamInfo,
+    DetailCommentInfo,
     Scroll,
   },
   created() {
@@ -69,6 +73,10 @@ export default {
         data.itemParams.info,
         data.itemParams.rule
       );
+      // 6.取出评论信息
+      if (data.rate.cRate !== 0) {
+        this.commentInfo = data.rate.list;
+      }
     });
   },
   methods: {
